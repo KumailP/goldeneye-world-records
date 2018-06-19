8dc9dbdd41b177db119fd450a51750d838bec1c7
KumailP <kumailpirzada@gmail.com>
2018-06-19 21:08:41 +0500

    Added some styling

--- src/components/MainTable.js
+++ src/components/MainTable.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Table, Icon, Modal, Button, Header, Form, Input } from 'semantic-ui-react'
 import records from '../tabledata/records'
-
+import './MainTable.css';
 const color = ['white', '#32661e', '#418e23', '#4dbc21', '#b2d6a4', '#e8b2b2', '#c47f7f', '#b25555', '#772828'];
 const levels = ['agent', 'secret', 'doubleo'];
 
@@ -55,6 +55,21 @@ export default class MainTable extends Component {
     })
   }
 
+  saveRecords = () => {
+
+    // send this.state.records to express backend which saves it to file :)
+
+
+
+  //   fs.writeFile("../tabledata/records.json", this.state.records, 'utf8', function (err) {
+  //     if (err) {
+  //         return console.log(err);
+  //     }
+  
+  //     console.log("The file was saved!");
+  // }); 
+  }
+
   render() {
 
     var tierOptions = [];
@@ -84,14 +99,14 @@ export default class MainTable extends Component {
 
 
                   {levels.map((val, i) => {
-                  return (<Modal dimmer="blurring" 
-                  trigger={<Table.Cell selectable 
-                   ><a href="#" onClick={this.savePrevious}>{record[val].time} {this.getIcon(record[val].tier)}</a></Table.Cell>}
+                  return (<Modal dimmer="blurring" key={i}
+                  trigger={<Table.Cell selectable className="cellStyle"
+                   onClick={this.savePrevious}>{record[val].time} {this.getIcon(record[val].tier)}</Table.Cell>}
                    basic size='small'>
                     <Header icon='edit' content='Edit Record' />
                     <Modal.Content>
                       <p>
-                        {record.stage} - {val.toUpperCase} Time
+                        {record.stage} - {val.charAt(0).toUpperCase() + val.slice(1)} Time
                       </p>
                       <Form>
                         <label>
@@ -126,7 +141,7 @@ export default class MainTable extends Component {
                 <Table.Row key={label}>
                   <Table.Cell style={{color: 'gold', fontWeight: 'bold'}}>{record.stage}</Table.Cell>
                   {levels.map((val, i) => {
-                    return <Table.Cell>{record[val].time} {this.getIcon(record[val].tier)}</Table.Cell>
+                    return <Table.Cell key={i}>{record[val].time} {this.getIcon(record[val].tier)}</Table.Cell>
                   })}
                 </Table.Row>
             )
@@ -136,6 +151,11 @@ export default class MainTable extends Component {
           
         </Table>
         
+        {this.state.enableEditing ? 
+        <div><Button floated='left' color='black' onClick={() => this.setState({enableEditing: false})}>Back</Button>
+        <Button floated='right' color='black' onClick={this.saveRecords}>Save Records</Button></div>
+        : 
+        <Button floated='right' color='black' onClick={() => this.setState({enableEditing: true})}>Edit</Button>}
         </div>
     )
   }
